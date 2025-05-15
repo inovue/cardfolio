ecd4dee126c8441e4906f567a5616d06bf4adf22
inovue <inovue@users.noreply.github.com>
2025-05-15 15:02:44 +0000

    deploy: 07fb10aee8c1dbe9689d8ef937f538ea3d253160

---
+++ static/counter-CKpTVusQ.js
@@ -0,0 +1,1 @@
+import{j as n,u as e}from"./client-C3wZZ2eS.js";import"./honox-island-CttClIeI.js";const o=function(){const[t,r]=e(0);return n("div",{children:[n("p",{class:"py-2 text-2xl",children:t}),n("button",{type:"button",class:"btn btn-primary",onClick:()=>r(t+1),children:"Increment"})]})},c=function(t){return n(o,{...t})},u=!0;export{u as __importing_islands,c as default};
---
+++ static/honox-island-CttClIeI.js
@@ -0,0 +1,1 @@
+import{c as e}from"./client-C3wZZ2eS.js";new TextEncoder;const n=Symbol(),o=Symbol();e({[n]:!1,[o]:!1});
